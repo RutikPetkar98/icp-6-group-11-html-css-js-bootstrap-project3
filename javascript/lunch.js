1295945a946f2b887824df6d5da8015c0a991002
NamrataDDeokar <namratadeokar253@gmail.com>
2024-01-17 11:22:53 +0530

    js function

--- javascript/lunch.js
+++ javascript/lunch.js
@@ -1,22 +1,95 @@
+function increaseQuantity1(params) {
+    const QuantityElement = document.getElementById("card-1");
 
-const QuantityElement = document.getElementById("product_text");
-        
-        function increaseQuantity(params) {
-            let Quantity = parseInt(QuantityElement.innerText);
-            if (Quantity < 10) {
-            QuantityElement.innerText = ++Quantity;
-            }
-            else{
-            alert("You odered more than 10 items")
-            }
-        }
-    
-        function decreaseQuantity(params) {
-            let Quantity = parseInt(QuantityElement.innerText);
-           if (Quantity > 1) {
-            QuantityElement.innerText = --Quantity;
-            }
-            else{
-            alert("You can't add less than 1 item")
-            }
-        }
+      let Quantity = parseInt(QuantityElement.innerText);
+      if (Quantity < 10) {
+      QuantityElement.innerText = ++Quantity;
+      }
+      else{
+      alert("You odered more than 10 items")
+      }
+  }
+
+  function decreaseQuantity1(params) {
+    const QuantityElement = document.getElementById("card-1");
+
+      let Quantity = parseInt(QuantityElement.innerText);
+     if (Quantity > 1) {
+      QuantityElement.innerText = --Quantity;
+      }
+      else{
+      alert("You can't add less than 1 item")
+      }
+  }
+
+  function increaseQuantity2(params) {
+    const QuantityElement = document.getElementById("card-2");
+
+      let Quantity = parseInt(QuantityElement.innerText);
+      if (Quantity < 10) {
+      QuantityElement.innerText = ++Quantity;
+      }
+      else{
+      alert("You odered more than 10 items")
+      }
+  }
+
+  function decreaseQuantity2(params) {
+    const QuantityElement = document.getElementById("card-2");
+
+      let Quantity = parseInt(QuantityElement.innerText);
+     if (Quantity > 1) {
+      QuantityElement.innerText = --Quantity;
+      }
+      else{
+      alert("You can't add less than 1 item")
+      }
+  }
+
+  function increaseQuantity3(params) {
+    const QuantityElement = document.getElementById("card-3");
+
+      let Quantity = parseInt(QuantityElement.innerText);
+      if (Quantity < 10) {
+      QuantityElement.innerText = ++Quantity;
+      }
+      else{
+      alert("You odered more than 10 items")
+      }
+  }
+
+  function decreaseQuantity3(params) {
+    const QuantityElement = document.getElementById("card-3");
+
+      let Quantity = parseInt(QuantityElement.innerText);
+     if (Quantity > 1) {
+      QuantityElement.innerText = --Quantity;
+      }
+      else{
+      alert("You can't add less than 1 item")
+      }
+  }
+
+  function increaseQuantity4(params) {
+    const QuantityElement = document.getElementById("card-4");
+
+      let Quantity = parseInt(QuantityElement.innerText);
+      if (Quantity < 10) {
+      QuantityElement.innerText = ++Quantity;
+      }
+      else{
+      alert("You odered more than 10 items")
+      }
+  }
+
+  function decreaseQuantity4(params) {
+    const QuantityElement = document.getElementById("card-4");
+
+      let Quantity = parseInt(QuantityElement.innerText);
+     if (Quantity > 1) {
+      QuantityElement.innerText = --Quantity;
+      }
+      else{
+      alert("You can't add less than 1 item")
+      }
+  }
