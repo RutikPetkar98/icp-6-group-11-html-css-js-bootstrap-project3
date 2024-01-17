7613338930af344be5d585dff1c4d0457395d796
RutikPetkar98 <rutikpetkar431@gmail.com>
2024-01-17 16:29:39 +0530

    add some changes

---
+++ javascript/contact.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    // You can add your logic to handle form submission here
+    alert('Form submitted successfully!');
+  });
